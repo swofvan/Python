039ccd0b73b393c622aad749e0bae02b85cf1beb
Mohammed Swofvan P <swofvanmhd@gmail.com>
2026-06-04 10:51:11 +0530

    Find largest number from list with max()

--- Find largest number from list.py
+++ Find largest number from list.py
@@ -2,10 +2,16 @@
 
 num = [10, 20, 15, 60, 45]
 
-largest = num[0]
+# largest = num[0]
 
-for n in num:
-    if n > largest:
-        largest = n
+# for n in num:
+#     if n > largest:
+#         largest = n
+
+# print(largest)
+
+# ---------------------------------------  or
+
+largest = max(num)
 
 print(largest)

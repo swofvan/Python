d142eed19ebe0698ac44b8dbbe3395abc6ee6590
Mohammed Swofvan P <swofvanmhd@gmail.com>
2026-06-20 20:40:19 +0530

    Print all Prime Number

---
+++ Print all Prime Number.py
@@ -0,0 +1,14 @@
+# Print all Prime Numbers in an Interval
+
+lower = 900
+upper = 1000
+
+
+for num in range(lower, upper + 1):
+   
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)

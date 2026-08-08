05e7cc7ac92c79401f94cad8c50eca74c888d652
Mohammed Swofvan P <swofvanmhd@gmail.com>
2026-08-08 10:28:02 +0530

    input validation added

--- rabbit and tartail.py
+++ rabbit and tartail.py
@@ -4,12 +4,25 @@ system_number = str(random.randrange(1000, 9999))
 
 user_input = input("Enter a 4 digit number: ")
 
+rabbits = []
+turtles = []
 
 print(f"System number = {system_number}")
 
-if user_input.isalpha or len(user_input) != 4:
+if not user_input.isdigit() or len(user_input) != 4:
     print("invalid input")
 
 else:
     if user_input == system_number:
         print("Winner")
+
+    for n in range(4):
+        if user_input[n] == system_number[n]:
+            rabbits.append(n)
+
+    for n in range(4):
+        if user_input[n] in system_number and user_input[n] not in rabbits:
+            turtles.append(n)
+
+    print(f"you got {len(turtles)} turtles")
+    print(f"youb got {len(rabbits)} rabbits")

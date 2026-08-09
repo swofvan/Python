d59480cfc543711fed72f68971f488c51757e407
Mohammed Swofvan P <swofvanmhd@gmail.com>
2026-08-09 21:26:39 +0530

    while loop added

--- rabbit and tartail.py
+++ rabbit and tartail.py
@@ -1,28 +1,43 @@
 import random
 
-system_number = str(random.randrange(1000, 9999))
 
-user_input = input("Enter a 4 digit number: ")
+while True:
 
-rabbits = []
-turtles = []
+    system_number = str(random.randrange(1000, 9999))
 
-print(f"System number = {system_number}")
+    user_input = input("Enter a 4 digit number: ")
 
-if not user_input.isdigit() or len(user_input) != 4:
-    print("invalid input")
+    rabbits = []
+    tortoise = []
 
-else:
-    if user_input == system_number:
-        print("Winner")
+    print(f"System number = {system_number}")
 
-    for n in range(4):
-        if user_input[n] == system_number[n]:
-            rabbits.append(n)
+    if not user_input.isdigit() or len(user_input) != 4:
+        print("invalid input")
 
-    for n in range(4):
-        if user_input[n] in system_number and user_input[n] not in rabbits:
-            turtles.append(n)
+    else:
 
-    print(f"you got {len(turtles)} turtles")
-    print(f"youb got {len(rabbits)} rabbits")
+        for n in range(4):
+            if user_input[n] == system_number[n]:
+                rabbits.append(n)
+
+        for n in range(4):
+            if user_input[n] in system_number and user_input[n] not in rabbits:
+                tortoise.append(n)
+
+        count_of_rabbit = len(rabbits)
+        count_of_tortoise = len(tortoise)
+
+        if user_input == system_number:
+            print("Winner")
+
+            continue_play = input(f"\n Do you want to continue? (y/n): ").lower
+
+            if continue_play == "y":
+                continue
+
+            else:
+                break
+
+        print(f"you got {count_of_tortoise} tortoise")
+        print(f"youb got {count_of_rabbit} rabbits")
